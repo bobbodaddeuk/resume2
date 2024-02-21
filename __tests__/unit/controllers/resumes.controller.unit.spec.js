5f403588dabbc2b91ad3dad888c76fe86b4d7769
TriumphOne <woo001232naver.com>
2024-02-21 16:50:24 +0900

    이력서 컨트롤러 계층 테스트 코드 찍먹

---
+++ __tests__/unit/controllers/resumes.controller.unit.spec.js
@@ -0,0 +1,19 @@
+// import { jest } from '@jest/globals'
+// import { ResumeController } from '../../../src/controllers/resume.controller'
+
+// let mockResumeService = {
+//     createResume: jest.fn(),
+//     getResumes: jest.fn(),
+//     getResumeById: jest.fn(),
+//     updateResume: jest.fn(),
+//     deleteResume: jest.fn(),
+// }
+
+// const mockRequest = {
+//     body: jest.fn(),
+// }
+
+// const mockResponse = {
+//     status: jest.fn(),
+//     json: jest.fn(),
+// }
